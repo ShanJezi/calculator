85b979ec01d694dc8f1f3edde7c61480a0f8dede
Shan Jezi <sobrien1028@gmail.com>
2023-01-14 10:53:42 -0800

    get button elements

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const sevenBtn = document.getElementById('7');
+const eightBtn = document.getElementById('8');
+const nineBtn = document.getElementById('9');
+const fourBtn = document.getElementById('4');
+const fiveBtn = document.getElementById('5');
+const sixBtn = document.getElementById('6');
+const oneBtn = document.getElementById('1');
+const twoBtn = document.getElementById('2');
+const threeBtn = document.getElementById('3');
+const decimalBtn = document.getElementById('decimal');
+const zeroBtn = document.getElementById('0');
+const posNegBtn = document.getElementById('pos-neg');
+
+const deleteBtn = document.getElementById('delete');
+const clearBtn = document.getElementById('clear');
+const multiplyBtn = document.getElementById('multiply');
+const divideBtn = document.getElementById('divide');
+const addBtn = document.getElementById('add');
+const subtractBtn = document.getElementById('subtract');
+const equalsBtn = document.getElementById('equals');
+
+
 function add(a, b) {
   return a + b;
 }
